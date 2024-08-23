d59f0d3528445fc08a812a530e91f9798d1e82db
SuryaKantTiwari <jamcocobutter@gmail.com>
2024-08-23 18:40:02 +0530

    nav-bar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/shared/Navbar";
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
